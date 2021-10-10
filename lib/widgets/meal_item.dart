82f007a1d6b2a19fd377d68f984357c33d4386ac
laila.nabil <laila.n.mostafa@gmail.com>
2021-10-10 23:55:11 +0200

    working but text when overflow goes left?!

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -11,19 +11,46 @@ class MealItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final double cardHeight = 250;
     return LayoutBuilder(
-      builder: (ctx,constraints){
+      builder: (ctx, constraints) {
         return Card(
-          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(15.0)),
           child: Stack(
             children: [
               Container(
-                decoration:
-                BoxDecoration(borderRadius: BorderRadius.circular(15.0)),
-                child: Image.network(
-                  currentMeal.imageUrl,
-                  height: cardHeight - 50,
-                  width: constraints.maxWidth,
-                  fit: BoxFit.fitWidth,
+                height: cardHeight - 50,
+                alignment: Alignment.centerRight,
+                child: Text(
+                  currentMeal.title,
+                  style: Theme.of(context).textTheme.headline5.copyWith(
+                        backgroundColor: Colors.black12,
+                      ),
+                ),
+              ),
+              Container(
+                height: cardHeight - 50,
+                width: constraints.maxWidth,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        fit: BoxFit.fitWidth,
+                        image: NetworkImage(currentMeal.imageUrl)),
+                    color: Colors.teal,
+                    borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(15.0),
+                        topLeft: Radius.circular(15.0))),
+                child: Container(
+                  width: constraints.maxWidth * 3 /2,
+                  alignment: Alignment.bottomRight,
+                  padding: EdgeInsets.only(bottom: 10),
+                  child: Text(
+                    currentMeal.title,
+                    style: Theme.of(context).textTheme.headline5.copyWith(
+                          backgroundColor: Colors.black45,
+                          color: Colors.grey.shade200,
+                          // fontSize: 30
+
+                        ),
+                  ),
                 ),
               ),
               Container(
